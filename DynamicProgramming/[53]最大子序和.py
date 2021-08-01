c10dc355b8b1cf9b6ef6d728dab1e49ad4d528ee
tangmengjie <tangxyw@hotmail.com>
2021-08-01 17:56:50 +0800

    modify[53]

--- DynamicProgramming/[53]最大子序和.py
+++ DynamicProgramming/[53]最大子序和.py
@@ -1,35 +1,21 @@
 from typing import List
 
-
 class Solution:
-    """
-    给定一个整数数组nums，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
-
-    示例:
-    输入: [-2,1,-3,4,-1,2,1,-5,4]
-    输出: 6
-    解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
-
-    """
-
-    @staticmethod
-    def maxSubArray(nums: List[int]) -> int:
-        """
-        动态规划解法
-        :param nums: 整数数组
-        :return: 连续子数组的最大和
-        """
-        # 定义dp table, 第i项代表以nums[i]为结尾的连续子数组的最大和
-        dp = [0 for _ in range(len(nums))]
-        # base case
+    def maxSubArray(self, nums: List[int]) -> int:
+        # 动态规划, 自底向上
+        # 定义dp数组, len(nums) = n
+        # 状态: dp[i]为以i为结尾的所有子数组的最大和, 故max(dp[i])为所求
+        # 选择: 在第i天：
+        #       1. 取i-1的结果加上nums[i], 即dp[i-1]+nums[i];
+        #       2. 只取nums[i]
+        # base case: dp[0] = nums[0]
+        n = len(nums)
+        dp = [0] * n
         dp[0] = nums[0]
-        # 存放最大值
         res = dp[0]
-        for i in range(1, len(nums)):
-            dp[i] = max(dp[i - 1] + nums[i], nums[i])  # 状态转移方程
-            res = max(dp[i], res)
+        for i in range(1, n):
+            dp[i] = max(dp[i-1] + nums[i],
+                        nums[i])
+            res = max(res, dp[i])
+
         return res
-
-
-if __name__ == "__main__":
-    print(Solution.maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
