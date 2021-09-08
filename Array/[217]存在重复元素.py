492bd70aeae313bcabba35df94fe6bfc29791c4f
tangmengjie <tangxyw@hotmail.com>
2021-09-08 21:55:37 +0800

    add[217]

---
+++ Array/[217]存在重复元素.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        s = set()
+        for num in nums:
+            if num in s:
+                return True
+            else:
+                s.add(num)
+
+        return False
