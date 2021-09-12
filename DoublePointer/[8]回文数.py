2aa9e26d1b8b60aa7d21c64fbffd5966803b19df
tangmengjie <tangxyw@hotmail.com>
2021-09-12 12:34:25 +0800

    add[8]

---
+++ DoublePointer/[8]回文数.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        # 双指针
+        if x < 0:
+            return False
+        x = str(x)
+        left = 0
+        right = len(x) - 1
+        while left < right:
+            if x[left] != x[right]:
+                return False
+            left += 1
+            right -= 1
+
+        return True
