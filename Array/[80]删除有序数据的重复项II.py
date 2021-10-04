fb80727892988add3a57d99e1d2e0ae24ad87f76
tangmengjie <tangxyw@hotmail.com>
2021-10-04 21:43:10 +0800

    modify[80]

--- Array/[80]删除有序数据的重复项II.py
+++ Array/[80]删除有序数据的重复项II.py
@@ -7,14 +7,14 @@ class Solution:
         # [26]删除有序数据的重复项 也可归到此范式下
         def sovle(k):
             p = 0  # 指向最终位置的指针
-            for num in nums:  # 遍历数组中所有元素
-                if p < k:  # 前k个元素都是合法的
+            for num in nums:  # 遍历数组中所有元素, 选择符合条件的放在指针p的位置
+                if p < k:  # 前k个元素都是合法的, 原地不动
                     p += 1
                     continue
                 elif nums[p - k] != num:  # 从第k+1个元素开始, 检查前面和ta隔着距离k的元素, 如果不等, 则将ta放到p的位置
                     nums[p] = num
                     p += 1  # 这时p往前走一步
-            # p为最终位置
+            # p-1为尾部位置索引, 所以长度为p
             return p
 
         return sovle(2)
