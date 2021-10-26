482b33c496f1ed7451a151e8f8957996ad3bcefb
tangmengjie <tangxyw@hotmail.com>
2021-10-26 17:45:51 +0800

    add[153]

---
+++ DivideAndConquer/[153]寻找旋转排序数组中的最小值.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        # 二分查找
+        # 双指针
+        left, right = 0, len(nums) - 1
+        while left < right:
+            mid = left + (right - left) // 2  # 当前数组长度为偶数时, mid靠近left
+            if nums[mid] < nums[right]:  # 中间位置元素小于右指针元素, 最小值在mid左边
+                right = mid  # mid也可能为最小值
+            elif nums[mid] > nums[right]:  # 中间位置元素大于右指针元素, 最小值在右边
+                left = mid + 1  # mid不可能为最小值
+
+        # 最终left和right指向同一个位置, 任选一个返回
+        return nums[left]
