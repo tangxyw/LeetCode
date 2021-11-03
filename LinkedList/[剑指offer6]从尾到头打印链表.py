7ee47df9d2262763ba8a9ada652dcf86402d6c1d
tangmengjie <tangxyw@hotmail.com>
2021-11-03 23:05:23 +0800

    add[剑指offer6]

---
+++ LinkedList/[剑指offer6]从尾到头打印链表.py
@@ -0,0 +1,18 @@
+from .LinkedList import ListNode
+from typing import List
+
+
+class Solution:
+    def reversePrint(self, head: ListNode) -> List[int]:
+        # 前序遍历
+        self.res = []
+
+        def preorder(head: ListNode):
+            if not head:  # base case, 到了末尾
+                return
+                # 递归下一个节点
+            preorder(head.next)
+            self.res.append(head.val)
+
+        dfs(head)
+        return self.res
