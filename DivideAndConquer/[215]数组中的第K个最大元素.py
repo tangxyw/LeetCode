ac9bf4e4eed82c8996cd0f9ad0e2ccc0d965355b
tangmengjie <tangxyw@hotmail.com>
2021-11-07 17:34:46 +0800

    modify[215]

--- DivideAndConquer/[215]数组中的第K个最大元素.py
+++ DivideAndConquer/[215]数组中的第K个最大元素.py
@@ -4,54 +4,33 @@ from typing import List
 
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        def partition(nums: List[int], left, right):
-            """将nums[left:right+1]中的某一个随机元素放到正确的降序排列位置上, 并返回位置索引"""
-            if left == right:
-                return left
-            seed = random.randint(left, right)  # 选取一个随机索引
-            nums[left], nums[seed] = nums[seed], nums[left]  # 再放到nums[left]位置上
-            pivot = nums[left]
+        def quick_sort(l, r):
+            """
+               预排序nums[l:r+1]:
+               随机取一个元素pivot, 放在正确的降序位置上, 使得pivot左边的元素都<=它, 右边的元素都>=它
+            """
+            if l >= r:  # base case
+                return
+            t = random.randint(l, r)  # 选取一个随机索引
+            nums[l], nums[t] = nums[t], nums[l]  # pivot放在最左边l位置
 
-            i = left  # 指针i先指向pivot
-            for j in range(left + 1, right + 1):  # 指针j从pivot后面开始遍历
-                if nums[j] > pivot:  # 只要遇到比pivot大, 就依次把它放到pivot后面
-                    nums[i + 1], nums[j] = nums[j], nums[i + 1]
-                    i += 1
-            # 上述遍历完成后, i指向最后一个比pivot大的元素, 交换它和pivot的位置, pivot就被放到了正确的排序位置上
-            # 此时pivot左边的元素都比它大, 右边的元素都小于等于它
-            nums[left], nums[i] = nums[i], nums[left]
-            return i
+            # 定义i, j双指针
+            i, j = l, r
+            while i < j:
+                while i < j and nums[j] <= nums[l]: j -= 1  # 左移j, 直到j右边都<=pivot, j停在>pivot的位置上
+                while i < j and nums[i] >= nums[l]: i += 1  # 右移i, 直到i左边都>=pivot, i停在<pivot的位置上
+                nums[i], nums[j] = nums[j], nums[i]  # 交换位置i和位置j的元素, 继续迭代
+            # 上述迭代完成后, i和j指向的位置相同, 该位置元素>pivot, 交换它们的位置, 使得pivot放在了正确的降序位置
+            # 此时,nums的前i+1个任意元素, 都大于等于nums后面任意元素
+            nums[l], nums[i] = nums[i], nums[l]
+            if k == i + 1:  # 前k个元素已经是最大的k个元素了, 直接返回
+                return
+            if k < i + 1:  # k小于i+1, 继续在nums的l到i-1区间递归
+                quick_sort(l, i - 1)
+            if k > i + 1:  # k大于i+1, 继续在nums的i+1到r区间递归
+                quick_sort(i + 1, r)
 
-        # 双指针
-        left = 0
-        right = len(nums) - 1
-        while left <= right:
-            print(nums)
-            index = partition(nums, left, right)  # 在指针范围内排序一个元素
-            print(index)
-            if index == k - 1:  # 由于index从0开始, 所以这里是k-1
-                return nums[index]  # 找到了就返回
-            elif index < k - 1:  # 找打了更大的数, 下一步在这个数的右边区间里找
-                left = index + 1
-            elif index > k - 1:  # 找到了更小的数, 下一步在这个数的左边区间里找
-                right = index - 1
-
-        """
-        另一种方法
-        pivot = nums[0]
-        equal = [x for x in nums if x == pivot]
-        lower = [x for x in nums if x < pivot]
-        greater = [x for x in nums if x > pivot]
-        if not lower and not greater:
-            return pivot
-
-        if len(greater) == k-1:
-            return pivot
-        elif len(greater) > k-1:
-            return self.findKthLargest(greater, k)
-        elif len(greater) < k-1:
-            if len(greater)+len(equal) >= k:
-                return pivot
-            else:
-                return self.findKthLargest(lower, k-len(greater)-len(equal))
-        """
+        # 从整个nums的首尾位置开始递归
+        quick_sort(0, len(nums) - 1)
+        #   递归结束后, 返回nums的第k个元素
+        return nums[k - 1]
