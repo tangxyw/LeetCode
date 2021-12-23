eead4c877db0c95305ba0b8ba721f775150d0ea1
tangmengjie <tangxyw@hotmail.com>
2021-12-23 22:22:40 +0800

    modify[101][剑指offer28]

--- BinaryTree/[101][剑指offer28]对称的二叉树.py
+++ BinaryTree/[101][剑指offer28]对称的二叉树.py
@@ -1,40 +1,22 @@
-from typing import List
-from CreateTree import TreeNode
+from .CreateTree import TreeNode
+
 
 class Solution:
     def isSymmetric(self, root: TreeNode) -> bool:
-        # 层次遍历, 广度优先
+        def issysmmetric(left, right):
+            """比较对称位置的两个节点"""
+            # 对称位置都为空
+            if not left and not right:
+                return True
+            # 对称位置一个为空另一个不为空
+            if (not left and right) or (not right and left):
+                return False
+            # 对称位置节点值不相等
+            if left.val != right.val:
+                return False
+            # 向下一层递归, 有两对对称位置
+            return issysmmetric(left.left, right.right) and issysmmetric(left.right, right.left)
+
         if not root:
             return True
-
-        def issys(l: List) -> bool:
-            """判断l是否为对称的"""
-            if not l:
-                return True
-            left = 0    # 左指针
-            right = len(l)-1    # 右指针
-            while left < right:
-                # 由于列表中可能有None, 且对称位置都是None的话结果为相等, 这里要注意判断顺序
-                if (not l[left] and l[right]) or (not l[right] and l[left]):
-                    break
-                elif (not l[left] and not l[right]) or l[left].val == l[right].val:
-                    left += 1   # 左指针右移
-                    right -= 1  # 右指针左移
-                else:
-                    break
-            return True if left > right else False  # 左指针在右指针右边说明没找到不对称的
-
-        q = [root]
-        while q:
-            tmp = []
-            for node in q:
-                left_node = node.left if node.left else None
-                right_node = node.right if node.right else None
-                # q中node的左右子节点都放进tmp, 无论是不是None
-                tmp.append(left_node)
-                tmp.append(right_node)
-            # 判断tmp是不是对称的
-            if issys(tmp) == False:
-                return False
-            q = [x for x in tmp if x]   # 把q中的None都排除
-        return True
+        return issysmmetric(root.left, root.right)
