e90e88574aebe3305800d23e2e8f9aab17e5c163
tangmengjie <tangxyw@hotmail.com>
2022-01-01 23:57:38 +0800

    modify[110][剑指offer55]

--- BinaryTree/[110][剑指offer55]平衡二叉树.py
+++ BinaryTree/[110][剑指offer55]平衡二叉树.py
@@ -3,21 +3,16 @@ from CreateTree import TreeNode
 
 class Solution:
     def isBalanced(self, root: TreeNode) -> bool:
-        self.flag = True
-
-        def getDepth(root):
-            """返回以节点为根节点的子树的深度, 判断左右子树的深度差"""
+        def helper(root: TreeNode):
+            """返回以节点为根节点的子树是否为高度平衡树, 和当前子树深度"""
+            # base case
             if not root:
-                return 0
-            if not self.flag:
-                return
-            left_depth = getDepth(root.left)
-            right_depth = getDepth(root.right)
-            print(left_depth, right_depth)
-            if abs(left_depth - right_depth) > 1:
-                # print(left_depth, right_depth)
-                self.flag = False
-            return max(left_depth, right_depth) + 1
+                return True, 0
+            # 后序遍历
+            left_flag, left_depth = helper(root.left)
+            right_flag, right_depth = helper(root.right)
+            # 当前左右子树都为高度平衡树, 且左右子树高度差<=1时, 当前子树为高度平衡树
+            return left_flag and right_flag and abs(left_depth - right_depth) <= 1, max(left_depth, right_depth) + 1
 
-        getDepth(root)
-        return self.flag
+        # 只返回第一个bool值即可
+        return helper(root)[0]
