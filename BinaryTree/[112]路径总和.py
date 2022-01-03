e139b1c01000e11529da046c8105ced5a2acb810
tangmengjie <tangxyw@hotmail.com>
2022-01-03 16:07:21 +0800

    modify[112]

--- BinaryTree/[112]路径总和.py
+++ BinaryTree/[112]路径总和.py
@@ -1,20 +1,16 @@
+from typing import Optional
 from CreateTree import TreeNode
 
-class Solution:
-    def hasPathSum(self, root: TreeNode, targetSum: int) -> bool:
-        def getPath(root: TreeNode, sumpath):
-            """返回根节点到当前节点的路径和"""
-            if not root:
-                return
-            if not root.left and not root.right:    # 当到达叶节点时, 判断路径和是否和目标值相等
-                if sumpath+root.val == targetSum:
-                    self.flag = True
-                return
-            getPath(root.left, sumpath+root.val)
-            getPath(root.right, sumpath+root.val)
 
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        # base case
         if not root:
             return False
-        self.flag = False
-        getPath(root, 0)
-        return self.flag
+        # 叶节点
+        if not root.left and not root.right:
+            return targetSum == root.val
+        # 递归左右子树
+        left = self.hasPathSum(root.left, targetSum - root.val)
+        right = self.hasPathSum(root.right, targetSum - root.val)
+        return left or right
