e0eb0c7af6641c883787af283f458566d1b77ea6
tangmengjie <tangxyw@hotmail.com>
2022-01-07 23:52:49 +0800

    modify[234]

--- LinkedList/[234]回文链表.py
+++ LinkedList/[234]回文链表.py
@@ -2,19 +2,26 @@ from .LinkedList import ListNode
 
 class Solution:
     def isPalindrome(self, head: ListNode) -> bool:
-        self.front_node = head
-        # 深度优先, 后序遍历, 链表可看做特殊的二叉树
-        def dfs(head: ListNode) -> bool:
-            if head:
-                # 先判断是不是False, 如果为False, 那么最终结果也是False
-                if not dfs(head.next):
-                    return False
-                # 判断对称位置的节点值是否相等, 不相等直接返回False
-                # 后序遍历, 所以先判断最后一个非空节点
-                if head.val != self.front_node.val:
-                    return False
-                # 以上两个判断都是True, 前面的节点后移1步
-                self.front_node = self.front_node.next
-            # head为空, 或者以上判断都为True, 返回True
+        # 前指针
+        self.front_pointer = head
+
+        def helper(head: ListNode):
+            # 后序遍历
+            # base case
+            if not head:
+                return True
+            # 一直向下递归
+            next_res = helper(head.next)
+            # 从倒数第一个节点开始
+            # 当前节点的后一个节点结果为False, 本层递归结果也为False, 最后抛出到顶层递归结果
+            if not next_res:
+                return False
+            # 当前节点值和对称位置节点值不等, 本层递归结果为False, 最后抛出到顶层递归结果
+            if head.val != self.front_pointer.val:
+                return False
+            # 前指针后移一位
+            self.front_pointer = self.front_pointer.next
+            # 上述if条件都不符合, 本层递归结果为True
             return True
-        return dfs(head)
+
+        return helper(head)
