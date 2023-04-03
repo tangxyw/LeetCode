639d2deccd0437ba734de4086df4dcfb2ec2d41a
tangxyw <tangxyw@hotmail.com>
2023-04-03 13:46:39 +0800

    optimize [78]子集 python版

--- python/Traceback/[78]子集.py
+++ python/Traceback/[78]子集.py
@@ -1,21 +1,49 @@
 from typing import List
 
 class Solution:
-    def subsets(self, nums: List[int]) -> List[List[int]]:
-        self.set = [True, False]    # 全部子集问题就是遍历每一个元素, 选择是否把该元素放到集合里, 找到所有可能, 实际也是树遍历问题
-        self.res = []
-        track = []  # 存放每一个子集, 也就是路径
 
-        def traceback(nums, track):
-            if not nums:    # 到达叶节点了
-                self.res.append(track[:])   # 注意append的必须是track的一个拷贝
-            else:
-                for flag in self.set:   # 选择是否将元素放入路径
-                    if flag:    # 如果是True就放入, 是False就什么都不做
-                        track.append(nums[0])
-                    traceback(nums[1:], track)  # 向下一个元素递归
-                    if flag:    # 如果本层将元素放入了路径, 那么就需要回溯, 没放入就不需要
-                        track.pop()
+    # 方法一
+    def subsets1(self, nums: List[int]) -> List[List[int]]:
+        # 全部子集问题就是遍历每一个元素, 选择是否把该元素放到集合里, 找到所有可能, 实际是遍历层数为nums长度的满二叉树问题
+        res = []
+        track = []
+        n = len(nums)
 
-        traceback(nums, track)
-        return self.res
+        def traceback(index, trace):
+            if index == n:
+                res.append(track[:])
+                return
+
+            # 选择当前index的值, 需要回溯
+            trace.append(nums[index])
+            traceback(index + 1, trace)
+            trace.pop()
+
+            # 不选择, 不需要回溯
+            traceback(index + 1, trace)
+
+        traceback(0, track)
+        return res
+
+    # 方法二
+    def subsets2(self, nums: List[int]) -> List[List[int]]:
+        # 与方法一不同，方法二在所有节点给出结果
+        # 方法二比方法一效率高
+        res = []
+        track = []
+        n = len(nums)
+
+        def traceback(index, trace):
+            # 从根节点开始, 每个中间节点都是结果
+            res.append(track[:])
+            if index == n:
+                return
+
+            for i in range(index, n):
+                trace.append(nums[i])
+                traceback(i + 1, trace)
+                trace.pop()
+
+        traceback(0, track)
+        return res
+
