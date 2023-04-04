2b35627c7829c0d33426201c7c37404bd88d1e5a
tangxyw <tangxyw@hotmail.com>
2023-04-04 15:52:29 +0800

    add [98]验证二叉搜索树 java版

---
+++ java/BinaryTree/[98]验证二叉搜索树/Solution.java
@@ -0,0 +1,15 @@
+import java.BinaryTree.TreeNode;
+
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    public boolean isValidBST(TreeNode root, long lower, long upper) {
+        if (root == null) return true;
+
+        if (root.val >= upper || root.val <= lower) return false;
+
+        return isValidBST(root.left, lower, root.val) && isValidBST(root.right, root.val, upper);
+    }
+}
