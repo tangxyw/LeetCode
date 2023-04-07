bc97f388cea7f24dd904eae1d12f5928b7212113
tangxyw <tangxyw@hotmail.com>
2023-04-07 14:36:53 +0800

    optimize [118]杨辉三角 python版

--- python/DynamicProgramming/[118]杨辉三角.py
+++ python/DynamicProgramming/[118]杨辉三角.py
@@ -1,16 +1,17 @@
 from typing import List
 
+
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        # 直接递推
-        # dp[i]为numRows=i+1的杨辉三角最后一行的结果
-        dp = [1]
-        res = [dp]
-        for i in range(1, numRows):
-            new_dp = [0] * (len(dp)+1)  # 需要增加的一行的元素数, 为上一个杨辉三角结果最后一行元素数+1
-            new_dp[0], new_dp[-1] = 1, 1    # 最左最右都是1
-            for j in range(1, len(new_dp)-1):   # 计算中间的数字
-                new_dp[j] = dp[j-1] + dp[j]
-            res.append(new_dp)  # 加入结果集
-            dp = new_dp # 保持dp为最后一行
+        res = []
+
+        for i in range(numRows):    # 迭代杨辉三角的行
+            layer_res = []  # 存放每行数字
+            for j in range(i + 1):  # 迭代每行的数字, 当前行长度为i+1
+                if j == 0 or j == i:    # 两端的数字都为1
+                    layer_res.append(1)
+                else:   # 注意迭代到i=2时才会涉及这里
+                    layer_res.append(res[i - 1][j - 1] + res[i - 1][j])
+            res.append(layer_res)
+
         return res
