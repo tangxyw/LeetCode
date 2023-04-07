51c88d01a7d1e0de754cbf83d7648d5c9ec1bafe
tangxyw <tangxyw@hotmail.com>
2023-04-07 14:37:58 +0800

    add [104]二叉树的最大深度 java版

---
+++ java/BinaryTree/[104]二叉树的最大深度/Solution.java
@@ -0,0 +1,13 @@
+import java.BinaryTree.TreeNode;
+
+class Solution {
+    public int maxDepth(TreeNode root) {
+        return maxDepth(root, 0);
+    }
+
+    private int maxDepth(TreeNode root, int depth) {
+        if (root == null) return depth;
+
+        return Math.max(maxDepth(root.left, depth+1), maxDepth(root.right, depth+1));
+    }
+}
