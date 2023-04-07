7f6b81493e9d63bb5cb5d2304e74e66dbe667fd9
tangxyw <tangxyw@hotmail.com>
2023-04-07 14:35:50 +0800

    optimize [108]将有序数组转换为二叉搜索树 python版

--- python/BinaryTree/[108]将有序数组转换为二叉搜索树.py
+++ python/BinaryTree/[108]将有序数组转换为二叉搜索树.py
@@ -1,13 +1,17 @@
-from typing import List
+from typing import List, Optional
 from CreateTree import TreeNode
 
+
 class Solution:
-    @staticmethod
-    def sortedArrayToBST(nums: List[int]) -> TreeNode:
-        if not nums:
-            return
-        mid = len(nums) // 2
-        root = TreeNode(nums[mid])
-        root.left = Solution.sortedArrayToBST(nums[:mid])
-        root.right = Solution.sortedArrayToBST(nums[(mid+1):])
-        return root
+    def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+        def helper(start: int, end: int) -> Optional[TreeNode]:
+            if start > end:
+                return None
+            mid = start + (end - start) // 2
+            node = TreeNode(nums[mid])
+            node.left = helper(start, mid - 1)
+            node.right = helper(mid + 1, end)
+
+            return node
+
+        return helper(0, len(nums) - 1)
