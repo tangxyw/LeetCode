7608b8d3289f51fe89857c13815dd0843613c226
tangxyw <tangxyw@hotmail.com>
2023-04-08 22:40:40 +0800

    optimize [122]买卖股票的最佳时机II python版

--- python/DynamicProgramming/[122]买卖股票的最佳时机II.py
+++ python/DynamicProgramming/[122]买卖股票的最佳时机II.py
@@ -1,11 +1,13 @@
 from typing import List
 
+
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         # 动态规划, 自底向上
         # 定义dp数组, len(prices) = n
-        # 状态: dp[0][i]为prices[:i], 持有cash, 前i天的最大收益(i从0开始,到n-1),
-        #       dp[1][i]为prices[:i], 持有stock, 前i天的最大收益(i从0开始, 到n-1),
+        # 每天交易结束后只可能存在手里有一支股票或者没有股票的状态
+        # 状态: dp[0][i]为第i天交易后, 手里没有股票的最大利润
+        #       dp[1][i]为第i天交易后, 手里持有股票的最大利润
         #       故dp[0][n-1]为所求
         # 选择: 在第i天：
         #       dp[0][i]:
@@ -21,7 +23,6 @@ class Solution:
         dp = [[0 for _ in range(n + 1)] for _ in range(2)]
         dp[0][0] = 0
         dp[1][0] = -prices[0]
-        print(dp)
         for i in range(1, n):
             dp[0][i] = max(dp[0][i-1],
                            dp[1][i-1] + prices[i])
