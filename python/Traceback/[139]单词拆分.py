dba1d63ce7f5e0d15ab0b18efbed1809d9e3952d
tangxyw <tangxyw@hotmail.com>
2023-04-10 15:10:06 +0800

    optimize [139]单词拆分 python版

--- python/Traceback/[139]单词拆分.py
+++ python/Traceback/[139]单词拆分.py
@@ -8,21 +8,22 @@ class Solution:
         memo = set()  # 备忘录, 存放无法进行拆分的子串start索引
         n = len(s)
 
-        def dfs(start):
+        def dfs(start: int) -> bool:
             if self.global_res:
-                return
+                return True
             if start == n:  # 可以拆分
                 self.global_res = True
-                return
-            if start in memo:  # 查找备忘录，如果找到, 剪纸
+                return True
+            if start in memo:  # 查找备忘录，如果找到, 剪枝
                 return False
 
             inner_res = False
             for i in range(start, n + 1):  # 从start开始遍历子串的前i个字符
                 if s[start:i] in wordDict:
                     inner_res = dfs(i) or inner_res  # 更新本次递归结果
-            if not inner_res:  # 本次递归结果为False或者None, 就将start加入到memo中
+            if not inner_res:  # 本次递归结果为False, 就将start加入到memo中
                 memo.add(start)
+                return False
 
         dfs(0)
         return self.global_res
