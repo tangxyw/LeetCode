37f5047893146c915b0fd27e8474f20bc364a759
tangxyw <tangxyw@hotmail.com>
2023-04-12 11:49:49 +0800

    optimize [162]寻找峰值 python版

--- python/DivideAndConquer/[162]寻找峰值.py
+++ python/DivideAndConquer/[162]寻找峰值.py
@@ -3,18 +3,24 @@ from typing import List
 
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
+        # 二分法
         n = len(nums)
-
         left = 0
         right = n - 1
 
+        def get(i: int) -> int:
+            """辅助函数, 处理索引边界"""
+            if i == -1 or i == n:
+                return float('-inf')
+            return nums[i]
+
         while left <= right:
-            if left == right:  # 到了数组边界
+            if left == right:
                 return left
             mid = (right - left) // 2 + left
-            if nums[mid] < nums[mid + 1]:  # mid右侧的数更大，右侧区间必存在峰值
-                left = mid + 1
-            elif nums[mid - 1] < nums[mid]:  # mid比mid+1大, 且也比mid-1大, mid就是峰值
+            if get(mid - 1) < get(mid) > get(mid + 1):  # 找到峰值返回
                 return mid
-            elif nums[mid - 1] > nums[mid]:  # mid左侧的数更大, 左侧区间必存在峰值
+            elif get(mid) < get(mid + 1):  # 上坡, 峰值在右侧
+                left = mid + 1
+            else:  # 下坡, 峰值在左侧
                 right = mid - 1
