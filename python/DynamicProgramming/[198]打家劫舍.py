db0b6ee42fea1b2baeb940d798c6396a4709c3a6
tangxyw <tangxyw@hotmail.com>
2023-04-13 11:48:59 +0800

    optimize [198]打家劫舍 python版

--- python/DynamicProgramming/[198]打家劫舍.py
+++ python/DynamicProgramming/[198]打家劫舍.py
@@ -4,12 +4,16 @@ class Solution:
     def rob(self, nums: List[int]) -> int:
         # 动态规划, 考虑自底向上递推
         # 定义dp数组
-        # 状态: dp[i]为打劫nums[i:]的最大收益, 故dp[0]为所求
+        # 状态: dp[i]为打劫nums[:i]的最大收益, 故dp[n]为所求
         # 选择: 在位置i, 有打劫nums[i]和不打劫2种选择
-        # base case: dp[n], dp[n+1] = 0, 0
+        # base case: dp[n], dp[1] = 0, nums[0]
+
         n = len(nums)
-        dp = [0] * (n + 2)  # 隐含了base case
-        for i in range(n - 1, -1, -1):  # 倒序遍历索引
-            dp[i] = max(nums[i] + dp[i + 2],  # 打劫nums[i]
-                        dp[i + 1])  # 不打劫nums[i]
-        return dp[0]
+        dp = [0] * (n + 1)
+
+        dp[1] = nums[0]
+
+        for i in range(2, n + 1):
+            dp[i] = max(dp[i-1], dp[i-2] + nums[i-1])
+
+        return dp[n]
