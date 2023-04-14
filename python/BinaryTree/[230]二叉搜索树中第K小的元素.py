27d885a238f6d318c96b0bf36373139701dc3f8d
tangxyw <tangxyw@hotmail.com>
2023-04-14 11:49:57 +0800

    optimize [230]二叉搜索树中第K小的元素 python版

--- python/BinaryTree/[230]二叉搜索树中第K小的元素.py
+++ python/BinaryTree/[230]二叉搜索树中第K小的元素.py
@@ -1,5 +1,6 @@
 from CreateTree import TreeNode
 
+
 class Solution:
     def kthSmallest(self, root: TreeNode, k: int) -> int:
         # BST的中序遍历为递增序列
@@ -7,11 +8,14 @@ class Solution:
             if not root:
                 return
             inorder(root.left)
-            if self.k == 1:
-                return root.val
-            self.k -= self.k
-            print(root.val)
+            self.k -= 1
+            if self.k == 0:
+                self.res = root.val
+                return
             inorder(root.right)
 
         self.k = k
-        return inorder(root)
+        self.res = None
+        inorder(root)
+
+        return self.res
