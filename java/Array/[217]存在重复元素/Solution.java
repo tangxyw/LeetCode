80b08772ac79173e1fe44670be07084ae2fe8833
tangxyw <tangxyw@hotmail.com>
2023-04-14 11:48:51 +0800

    add [217]存在重复元素 java版

---
+++ java/Array/[217]存在重复元素/Solution.java
@@ -0,0 +1,14 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (set.contains(num)) return true;
+            set.add(num);
+        }
+
+        return false;
+    }
+}
