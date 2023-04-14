545b65dccd92747965c17fa4d06d7608903b39a1
tangxyw <tangxyw@hotmail.com>
2023-04-14 11:50:10 +0800

    optimize [215]数组中的第K个最大元素 python版

--- python/DivideAndConquer/[215]数组中的第K个最大元素.py
+++ python/DivideAndConquer/[215]数组中的第K个最大元素.py
@@ -4,31 +4,34 @@ from typing import List
 
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        def quick_sort(l, r):
+        def quick_sort(start, end):
             """
-               预排序nums[l:r+1]:
+               预排序nums[start:end+1]:
                随机取一个元素pivot, 放在正确的降序位置上, 使得pivot左边的元素都<=它, 右边的元素都>=它
             """
-            if l >= r:  # base case
+            if start == end:  # base case
                 return
-            t = random.randint(l, r)  # 选取一个随机索引
-            nums[l], nums[t] = nums[t], nums[l]  # pivot放在最左边l位置
+            t = random.randint(start, end)  # 选取一个随机索引
+            pivot = nums[t]
+            nums[start], nums[t] = nums[t], nums[start]  # pivot放在最左边l位置
 
             # 定义i, j双指针
-            i, j = l, r
+            i, j = start, end
             while i < j:
-                while i < j and nums[j] <= nums[l]: j -= 1  # 左移j, 直到j右边都<=pivot, j停在>pivot的位置上
-                while i < j and nums[i] >= nums[l]: i += 1  # 右移i, 直到i左边都>=pivot, i停在<pivot的位置上
+                while i < j and nums[j] <= pivot:
+                    j -= 1  # 左移j, 直到j右边都<=pivot, j停在>pivot的位置上
+                while i < j and nums[i] >= pivot:
+                    i += 1  # 右移i, 直到i左边都>=pivot, i停在<pivot的位置上
                 nums[i], nums[j] = nums[j], nums[i]  # 交换位置i和位置j的元素, 继续迭代
-            # 上述迭代完成后, i和j指向的位置相同, 该位置元素>pivot, 交换它们的位置, 使得pivot放在了正确的降序位置
+            # 上述迭代完成后, i和j指向的位置相同, 该位置元素>pivot(因为先移动j后移动i), 交换它们的位置, 使得pivot放在了正确的降序位置
             # 此时,nums的前i+1个任意元素, 都大于等于nums后面任意元素
-            nums[l], nums[i] = nums[i], nums[l]
-            if k == i + 1:  # 前k个元素已经是最大的k个元素了, 直接返回
+            nums[start], nums[i] = nums[i], nums[start]
+            if k - 1 == i:  # 前k个元素已经是最大的k个元素了, 直接返回
                 return
-            if k < i + 1:  # k小于i+1, 继续在nums的l到i-1区间递归
-                quick_sort(l, i - 1)
-            if k > i + 1:  # k大于i+1, 继续在nums的i+1到r区间递归
-                quick_sort(i + 1, r)
+            if k - 1 < i:  # k-1小于i, 继续在nums的l到i-1区间递归
+                quick_sort(start, i - 1)
+            if k - 1 > i:  # k-1大于i, 继续在nums的i+1到r区间递归
+                quick_sort(i + 1, end)
 
         # 从整个nums的首尾位置开始递归
         quick_sort(0, len(nums) - 1)
