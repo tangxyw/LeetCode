527d2e3076e6a42a05fc92f5e445cd6850632df2
tangxyw <tangxyw@hotmail.com>
2023-04-15 23:21:26 +0800

    add [237]删除链表中的节点 java版

---
+++ java/LinkedList/[237]删除链表中的节点/Solution.java
@@ -0,0 +1,11 @@
+import LinkedList.ListNode;
+
+class Solution {
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+        
+        return;
+        
+    }
+}
