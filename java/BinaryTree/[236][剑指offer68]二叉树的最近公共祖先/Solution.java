7c5b6a3e6b2ff4f5c55a478097d4509e94c61499
tangxyw <tangxyw@hotmail.com>
2023-04-15 23:20:56 +0800

    add [236][剑指offer68]二叉树的最近公共祖先 java版

---
+++ java/BinaryTree/[236][剑指offer68]二叉树的最近公共祖先/Solution.java
@@ -0,0 +1,16 @@
+import BinaryTree.TreeNode;
+
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) return root;
+        if (root == p || root == q) return root == p ? p : q;
+        
+        TreeNode left = lowestCommonAncestor(root.left,  p,  q);
+        TreeNode right = lowestCommonAncestor(root.right,  p,  q);
+
+        
+        if (left != null && right != null) return root;
+
+        return left == null ? right : left;
+    }
+}
