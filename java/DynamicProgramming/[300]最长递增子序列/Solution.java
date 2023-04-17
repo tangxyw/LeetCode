e5e5847552c4ba84dc3d028f3cb1597540e420a8
tangxyw <tangxyw@hotmail.com>
2023-04-17 11:20:12 +0800

    add [300]最长递增子序列 java版

---
+++ java/DynamicProgramming/[300]最长递增子序列/Solution.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+        int res = dp[0];
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            res = Math.max(res, dp[i]);
+        }
+
+        return res;
+    }
+}
