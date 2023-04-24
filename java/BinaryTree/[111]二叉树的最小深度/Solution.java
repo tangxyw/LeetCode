a607b156dd8bfe1096066a5b877372c5c7ba88f3
tangxyw <tangxyw@hotmail.com>
2023-04-24 11:19:44 +0800

    add [111]二叉树的最小深度 java版

---
+++ java/BinaryTree/[111]二叉树的最小深度/Solution.java
@@ -0,0 +1,15 @@
+import BinaryTree.TreeNode;
+
+class Solution {
+    public int minDepth(TreeNode root) {
+        if (root == null) return 0;
+
+        int left = minDepth(root.left);
+        int right = minDepth(root.right);
+
+        if (root.left == null || root.right == null) return left + right + 1;
+
+        return Math.min(left, right) + 1;
+
+    }
+}
