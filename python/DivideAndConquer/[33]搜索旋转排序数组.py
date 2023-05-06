b41aca940295604505374bcda0e0c1afe6c863cf
tangxyw <tangxyw@hotmail.com>
2023-05-06 11:06:59 +0800

    optimize [33]搜索旋转排序数组 python版

--- python/DivideAndConquer/[33]搜索旋转排序数组.py
+++ python/DivideAndConquer/[33]搜索旋转排序数组.py
@@ -3,23 +3,22 @@ from typing import List
 
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        # 分治, 二分搜索
-        def helper(start, end) -> int:
-            # base case, 只剩一个值判断是否为target
-            if start == end:
-                if nums[start] == target:
-                    return start
-                else:
-                    return -1
+        # 二分查找
+        left, right = 0, len(nums) - 1
 
-            # 升序部分, 由最大最小值判断target是否在该区间内, 相当与剪枝
-            if nums[start] < nums[end] and (target < nums[start] or target > nums[end]):
-                return -1
+        while left <= right:
+            mid = left + (right - left) // 2
+            if nums[mid] == target:
+                return mid
+            if nums[left] <= nums[mid]:  # 左侧是有序的(注意这里要加=, 因为mid是中间靠左的)
+                if nums[left] <= target < nums[mid]:  # target在有序的片段里
+                    right = mid - 1
+                else:  # target不在有序的片段里
+                    left = mid + 1
+            else:  # 左侧是无序的, 右侧必是有序的
+                if nums[mid] < target <= nums[right]:  # target在有序的片段里
+                    left = mid + 1
+                else:  # target不在有序的片段里
+                    right = mid - 1
 
-            mid = (end - start) // 2 + start  # 取区间中点
-            # 左右子数组递归
-            left = helper(start, mid)
-            right = helper(mid + 1, end)
-            return max(left, right)  # 若都没找到就是-1, 找到了就是target索引
-
-        return helper(0, len(nums) - 1)
+        return -1
