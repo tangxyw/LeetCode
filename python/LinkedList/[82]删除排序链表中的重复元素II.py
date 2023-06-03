899d41c1437b4735260605ef7b0f394bf9e1b356
tangxyw <tangxyw@hotmail.com>
2023-06-03 10:02:04 +0800

    optimize [82]删除排序链表中的重复元素II python版

--- python/LinkedList/[82]删除排序链表中的重复元素II.py
+++ python/LinkedList/[82]删除排序链表中的重复元素II.py
@@ -3,16 +3,21 @@ from .LinkedList import ListNode
 
 class Solution:
     def deleteDuplicates(self, head: ListNode) -> ListNode:
+        if not head or not head.next:
+            return head
+
         dummy = ListNode(-1)
         dummy.next = head
-        cur = dummy  # cur初始化为哑元节点, 保证在迭代中, cur的值不会等于后面节点的值
+        pre, cur = dummy, head
 
-        while cur.next and cur.next.next:  # 遍历直到倒数第三个节点
-            if cur.next.val == cur.next.next.val:  # cur的后两个节点值相等时
-                cur_val = cur.next.val
-                while cur.next and cur.next.val == cur_val:
-                    cur.next = cur.next.next  # 直到cur.next指向值不等于cur_value的节点
+        while cur:
+            # cur.next存在, 且和下一个节点的val相同
+            while cur.next and cur.val == cur.next.val:
+                cur = cur.next  # cur移动到重复值的最后一个位置
+            if pre.next == cur:  # 巧妙判断是否前面有重复值
+                pre = pre.next  # 如果没有, pre后移
             else:
-                cur = cur.next
+                pre.next = cur.next  # 如果有, pre指向最后一个重复值的后面
+            cur = cur.next  # cur后移
 
         return dummy.next
