f754e765e2aee7cd726b61ccb5fc159f417fc9f1
tangxyw <tangxyw@hotmail.com>
2023-06-14 11:26:56 +0800

    add [662]二叉树的最大宽度 python版

---
+++ python/BinaryTree/[662]二叉树最大宽度.py
@@ -0,0 +1,24 @@
+from typing import Optional
+from CreateTree import TreeNode
+
+
+class Solution:
+    def widthOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        res = 0
+
+        if not root:
+            return res
+
+        q = [(root, 1)]
+
+        while q:
+            tmp = []
+            for node, index in q:   # 按层给每个node从左到右编号
+                if node.left:
+                    tmp.append((node.left, index * 2))
+                if node.right:
+                    tmp.append((node.right, index * 2 + 1))
+            res = max(res, q[-1][1] - q[0][1] + 1)
+            q = tmp
+
+        return res
