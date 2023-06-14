b11c5d630222cf0463c62b72c94e2ced817c2d27
tangxyw <tangxyw@hotmail.com>
2023-06-14 15:25:42 +0800

    optimize [153]寻找旋转排序数组中的最小值 python版

--- python/DivideAndConquer/[153]寻找旋转排序数组中的最小值.py
+++ python/DivideAndConquer/[153]寻找旋转排序数组中的最小值.py
@@ -8,9 +8,9 @@ class Solution:
         left, right = 0, len(nums) - 1
         while left < right:
             mid = left + (right - left) // 2  # 当前数组长度为偶数时, mid靠近left
-            if nums[mid] < nums[right]:  # 中间位置元素小于右指针元素, 最小值在mid左边
+            if nums[mid] < nums[-1]:  # 中间位置元素小于nums最右元素, 最小值在mid左边
                 right = mid  # mid也可能为最小值
-            elif nums[mid] > nums[right]:  # 中间位置元素大于右指针元素, 最小值在右边
+            else:  # 中间位置元素大于右指针元素, 最小值在右边
                 left = mid + 1  # mid不可能为最小值
 
         # 最终left和right指向同一个位置, 任选一个返回
