7b2b4feedb70f41bae2eec8f4664e9065485ad38
tangxyw <tangxyw@hotmail.com>
2023-08-04 15:01:41 +0800

    optimize [239]滑动窗口最大值 python版

--- python/Array/[239]滑动窗口最大值.py
+++ python/Array/[239]滑动窗口最大值.py
@@ -8,18 +8,15 @@ class Solution:
         # 一共有n-k+1个窗口
         res = []
         q = deque()  # 存放索引, 索引是单调递增的, 索引对应的值是单调递减的, 每迭代1个窗口, 都返回q[0]为窗口内最大元素
-        for i in range(k):  # 首先处理前k个元素, 即第1个窗口
+        for i in range(len(nums)):  # i为窗口的右端点
             while q and nums[i] >= nums[q[-1]]:  # q非空且当前值>=队列末尾元素
                 q.pop()  # 弹出队列末尾元素
             q.append(i)  # 将当前索引加入队列
-        res.append(nums[q[0]])
 
-        for i in range(k, len(nums)):  # 从第2个窗口开始遍历, i为窗口最后一个元素的索引
-            while q and nums[i] >= nums[q[-1]]:  # q非空且当前值>=队列末尾元素
-                q.pop()  # 弹出队列末尾元素
-            q.append(i)  # 将当前索引加入队列
-            while q[0] <= i - k:  # 若q[0]已经不在当前窗口, 则从q左侧弹出
-                q.popleft()
-            res.append(nums[q[0]])  # 这时的q[0]已经在当前窗口内, 且是窗口最大值
+            while q[0] <= i - k:  # q[0]不在当前窗口
+                q.popleft()  # 弹出
+
+            if i >= k - 1:  # i为k-1时才是第一个窗口
+                res.append(nums[q[0]])
 
         return res
