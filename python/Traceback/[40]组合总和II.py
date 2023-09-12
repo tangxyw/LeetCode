b245cd8601c0a10d0e83c5088b44bb9670b05202
tangxyw <tangxyw@hotmail.com>
2023-09-12 12:19:30 +0800

    optimize [40]组合总和II python版

--- python/Traceback/[40]组合总和II.py
+++ python/Traceback/[40]组合总和II.py
@@ -3,25 +3,25 @@ from typing import List
 
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
-        # https://leetcode-cn.com/problems/combination-sum-ii/solution/hui-su-suan-fa-jian-zhi-python-dai-ma-java-dai-m-3/
-        # 深度优先遍历+回溯+剪枝
-        self.res = []
+        candidates.sort()
+        n = len(candidates)
         track = []
-        candidates.sort()  # 将candidates升序排列, 避免重复
+        res = []
 
-        def traceback(s: int, start: int, track: List[int]):
-            # start为candidates索引
-            if s == target:
-                self.res.append(track[:])
-                return
-            if s > target:
+        def traceback(start: int, track: List[int], target: int) -> None:
+            if target == 0:
+                res.append(track[:])
                 return
 
-            for i in range(start, len(candidates)):  # 从start开始遍历
-                if i == start or candidates[i] > candidates[i - 1]:  # 如果有重复数值, 则只对第一个出现的进行递归, 其他的被剪枝
-                    track.append(candidates[i])  # 将符合条件的数值放进track
-                    traceback(s + candidates[i], i + 1, track)  # 更新s和start, 向下递归
-                    track.pop()  # 回溯
+            for i in range(start, n):
+                if i > start and candidates[i - 1] == candidates[i]:    # 剪枝
+                    continue
+                if candidates[i] <= target:
+                    track.append(candidates[i])
+                    traceback(i + 1, track, target - candidates[i])
+                    track.pop()
 
-        traceback(0, 0, track)
-        return self.res
+        traceback(0, track, target)
+
+        return res
+
