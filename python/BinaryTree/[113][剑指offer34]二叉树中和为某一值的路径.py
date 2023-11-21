1d64985ecb7723ebb0ef07b30a8645babc445978
tangxyw <tangxyw@hotmail.com>
2023-11-21 13:36:36 +0800

    optimize [113][剑指offer34]二叉树中和为某一值的路径 python版

--- python/BinaryTree/[113][剑指offer34]二叉树中和为某一值的路径.py
+++ python/BinaryTree/[113][剑指offer34]二叉树中和为某一值的路径.py
@@ -1,16 +1,28 @@
-from typing import List
+from typing import List, Optional
 from CreateTree import TreeNode
 
 
 class Solution:
-    def pathSum(self, root: TreeNode, target: int) -> List[List[int]]:
-        def getPathSum(root: TreeNode, curpath: List[int]):
-            if not root:
-                return []   # 越过叶节点, 返回空列表
-            if not root.left and not root.right:    # 到达叶节点,
-                if root.val + sum(curpath) == target:   # 判断当前路径和+当前节点值是否等于目标值
-                    return [curpath +[root.val]] # 等于目标值, 返回该路径列表
-            # 在左右子树递归, 当前节点的结果为左右子树的返回列表拼接
-            return getPathSum(root.left, curpath +[root.val]) + getPathSum(root.right, curpath +[root.val])
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
+        track = []
+        res = []
 
-        return getPathSum(root, [])
+        def traceback(root: Optional[TreeNode], track: List[int], target: int) -> None:
+            if not root:
+                return
+
+            # 根节点
+            if not root.left and not root.right:
+                if root.val == target:
+                    res.append(track + [root.val])
+                return
+
+            # 递归&回溯
+            track.append(root.val)
+            traceback(root.left, track, target - root.val)
+            traceback(root.right, track, target - root.val)
+            track.pop()
+
+        traceback(root, track, targetSum)
+
+        return res
